d92df5d26a28b8bbde28dfc5c77a47f5ba4ab29e
Mohit Kukreja <mohitk1905@gmail.com>
2019-04-01 00:59:37 -0400

    pkg.json

--- cliend/src/app/app.module.ts
+++ cliend/src/app/app.module.ts
@@ -1,45 +1,49 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { routing } from './app-routing';
-import { ReactiveFormsModule } from '@angular/forms';
-import { FormsModule,Validators } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-import { HttpClientModule } from '@angular/common/http';
-import { UUID } from 'angular2-uuid';
-import { AppComponent } from './app.component';
-import { RegisterComponent } from './register/register.component';
-import { AuthenticationService } from './services/authentication.service';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { routing } from "./app-routing";
+import { ReactiveFormsModule } from "@angular/forms";
+import { FormsModule, Validators } from "@angular/forms";
+import { HttpModule } from "@angular/http";
+import { HttpClientModule } from "@angular/common/http";
+import { UUID } from "angular2-uuid";
+import { AppComponent } from "./app.component";
+import { RegisterComponent } from "./register/register.component";
+import { AuthenticationService } from "./services/authentication.service";
 // import { LoginComponent } from './login/login.component';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from "./login/login.component";
 // import { SearchComponent } from './search/search.component';
-import { PlanComponent } from './plan/plan.component';
-import { UsertripService } from './services/usertrip.service';
-import { LoneComponent } from './lone/lone.component';
-import { MytripsComponent } from './mytrips/mytrips.component';
-import { ModalComponent } from './services/modal.component';
-import { GroupComponent } from './group/group.component';
-import {PlanService} from './services/plan.service';
-import {TabsModule} from "ngx-tabs";
-import {TransactionService} from './services/transaction.service'
-import { MapsAPILoader,AgmCoreModule } from '@agm/core';
+import { PlanComponent } from "./plan/plan.component";
+import { UsertripService } from "./services/usertrip.service";
+import { LoneComponent } from "./lone/lone.component";
+import { MytripsComponent } from "./mytrips/mytrips.component";
+import { ModalComponent } from "./services/modal.component";
+import { GroupComponent } from "./group/group.component";
+import { PlanService } from "./services/plan.service";
+import { TabsModule } from "ngx-tabs";
+import { TransactionService } from "./services/transaction.service";
+import { MapsAPILoader, AgmCoreModule } from "@agm/core";
+import { MapValuesPipe } from "./services/MapService";
 //import { AgmCoreModule } from '@agm/core';
-import { SuggestionsService } from './services/suggestions.service';
-import { NavbarComponent } from './navbar/navbar.component';
-import { NavbarService } from './services/navbar.service';
-import { HomeComponent } from './home/home.component';
-import { SearchComponent } from './search/search.component';
-import { ChatComponent } from './chat/chat.component';
+import { SuggestionsService } from "./services/suggestions.service";
+import { NavbarComponent } from "./navbar/navbar.component";
+import { NavbarService } from "./services/navbar.service";
+import { HomeComponent } from "./home/home.component";
+import { SearchComponent } from "./search/search.component";
+import { ChatComponent } from "./chat/chat.component";
 //import { NavbarService } from './navbar/navbar.service';
-import {ChatService} from './services/chat.service';
-import { ImageGalleryComponent } from './image-gallery/image-gallery.component';
-import { FileuploadComponent } from './fileupload/fileupload.component';
-import { FileService } from './services/file.service';
-import { FileSelectDirective, FileUploader, FileUploadModule} from 'ng2-file-upload';
-import { SuggestionComponent } from './suggestion/suggestion.component';
-import { BudgetComponent } from './budget/budget.component';
-import { MessagesComponent } from './messages/messages.component';
-import { HashLocationStrategy, LocationStrategy } from '@angular/common';
-
+import { ChatService } from "./services/chat.service";
+import { ImageGalleryComponent } from "./image-gallery/image-gallery.component";
+import { FileuploadComponent } from "./fileupload/fileupload.component";
+import { FileService } from "./services/file.service";
+import {
+  FileSelectDirective,
+  FileUploader,
+  FileUploadModule
+} from "ng2-file-upload";
+import { SuggestionComponent } from "./suggestion/suggestion.component";
+import { BudgetComponent } from "./budget/budget.component";
+import { MessagesComponent } from "./messages/messages.component";
+import { HashLocationStrategy, LocationStrategy } from "@angular/common";
 
 @NgModule({
   declarations: [
@@ -60,8 +64,8 @@ import { HashLocationStrategy, LocationStrategy } from '@angular/common';
     FileuploadComponent,
     SuggestionComponent,
     BudgetComponent,
-    MessagesComponent
-    
+    MessagesComponent,
+    MapValuesPipe
   ],
   imports: [
     BrowserModule,
@@ -72,7 +76,10 @@ import { HashLocationStrategy, LocationStrategy } from '@angular/common';
     FileUploadModule,
     HttpModule,
     TabsModule,
-    AgmCoreModule.forRoot({apiKey:'AIzaSyDgQfG6Y-bbidUjlgoNX8SotR2ofd2H9kA',libraries:["places"]})
+    AgmCoreModule.forRoot({
+      apiKey: "AIzaSyDgQfG6Y-bbidUjlgoNX8SotR2ofd2H9kA",
+      libraries: ["places"]
+    })
   ],
   providers: [
     AuthenticationService,
@@ -84,8 +91,8 @@ import { HashLocationStrategy, LocationStrategy } from '@angular/common';
     NavbarService,
     ChatService,
     FileService,
-    {provide:LocationStrategy,useClass:HashLocationStrategy}
+    { provide: LocationStrategy, useClass: HashLocationStrategy }
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
