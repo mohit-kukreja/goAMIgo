2e19d6d7b51a0636220ab12d0e6f1e96518bb5cb
Mohit Kukreja <mohitk1905@gmail.com>
2019-04-01 01:22:39 -0400

    error reslove prop

--- cliend/src/app/home/home.component.ts
+++ cliend/src/app/home/home.component.ts
@@ -1,28 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
-
-import { NavbarService } from './../services/navbar.service';
+import { NavbarService } from "./../services/navbar.service";
 @Component({
-    moduleId: module.id.toString(),
-    templateUrl: 'home.component.html'
+  moduleId: module.id.toString(),
+  templateUrl: "home.component.html"
 })
-
 export class HomeComponent implements OnInit {
-   
-    private username = "";
+  username = "";
 
-    constructor(public nav: NavbarService) {
-       // this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
-    }
+  constructor(public nav: NavbarService) {
+    // this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
+  }
 
-    ngOnInit() {
-        this.nav.show();
-        this.loadAllUsers();
-        this.username = sessionStorage.getItem('userName');
+  ngOnInit() {
+    this.nav.show();
+    this.loadAllUsers();
+    this.username = sessionStorage.getItem("userName");
+  }
 
-    }
-
-    private loadAllUsers() {
-       // this.userService.getAll().subscribe(users => { this.users = users; });
-    }
+  private loadAllUsers() {
+    // this.userService.getAll().subscribe(users => { this.users = users; });
+  }
 }
